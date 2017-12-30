160a79aa391c13c3e31057be1c6f679e4a78fce6
Jonathan Amsterdam <jba@google.com>
2017-12-30 04:49:52 -0500

    basic template logic

--- quiz_main.go
+++ quiz_main.go
@@ -5,13 +5,20 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
 )
 
-var dataFile = flag.String("i", "", "file of data")
+var (
+	itemFile    = flag.String("i", "items.txt", "file of items")
+	lexiconFile = flag.String("l", "lexicon.txt", "file of words")
+)
 
-var input *bufio.Reader
+var (
+	input   *bufio.Reader
+	lexicon map[string][]*Word // part of speech to word
+)
 
 type Item struct {
 	English    string
@@ -19,12 +26,23 @@ type Item struct {
 	Characters string
 }
 
+type Word struct {
+	English      string
+	Pinyin       string
+	Characters   string
+	PartOfSpeech string
+}
+
 func main() {
 	flag.Parse()
-	if *dataFile == "" {
+	if *itemFile == "" {
 		log.Fatal("need -i")
 	}
-	items, err := readDataFile(*dataFile)
+	items, err := readDataFile(*itemFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lexicon, err = readLexicon(*lexiconFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,34 +52,36 @@ func main() {
 }
 
 func readDataFile(filename string) ([]*Item, error) {
-	f, err := os.Open(filename)
+	lines, err := readTSVFile(filename, 3)
 	if err != nil {
 		return nil, err
 	}
-	defer f.Close()
 	var items []*Item
-	s := bufio.NewScanner(f)
-	n := 0
-	for s.Scan() {
-		n++
-		line := strings.TrimSpace(s.Text())
-		if line == "" || line[0] == '#' {
-			continue
-		}
-		parts := strings.Split(s.Text(), "\t")
-		if len(parts) != 3 {
-			return nil, fmt.Errorf("%s:%d: need 3 parts", filename, n)
-		}
+	for _, parts := range lines {
 		items = append(items, &Item{
 			English:    parts[0],
 			Pinyin:     parts[1],
 			Characters: parts[2],
 		})
 	}
-	if s.Err() != nil {
-		return nil, s.Err()
-	}
 	return items, nil
+}
+
+func readLexicon(filename string) (map[string][]*Word, error) {
+	lines, err := readTSVFile(filename, 3)
+	if err != nil {
+		return nil, err
+	}
+	lex := map[string][]*Word{}
+	for _, line := range lines {
+		word := &Word{
+			English:      line[0],
+			Pinyin:       line[1],
+			PartOfSpeech: line[2],
+		}
+		lex[word.PartOfSpeech] = append(lex[word.PartOfSpeech], word)
+	}
+	return lex, nil
 }
 
 func quiz(items []*Item) {
@@ -73,15 +93,85 @@ func quiz(items []*Item) {
 	for len(unfinished) > 0 {
 		fmt.Printf("%d items to study.\n", len(unfinished))
 		for item := range unfinished {
-			fmt.Printf("%s  ", item.English)
+			question, answer := qa(item)
+			fmt.Printf("%-30s ", question)
 			input.ReadString('\n')
-			fmt.Printf("%s\t", item.Pinyin)
+			fmt.Printf("%-30s ", answer)
 			if yorn("y/n? ") {
 				delete(unfinished, item)
 			}
 		}
 		fmt.Println()
 	}
+}
+
+func qa(item *Item) (q, a string) {
+	if !isTemplate(item.English) {
+		return item.English, item.Pinyin
+	}
+	q, bindings := instantiateTemplate(item.English, lexicon)
+	a = applyBindings(item.Pinyin, bindings)
+	return q, a
+}
+
+func isTemplate(s string) bool {
+	for _, w := range strings.Fields(s) {
+		if strings.HasPrefix(w, ":") {
+			return true
+		}
+	}
+	return false
+}
+
+func instantiateTemplate(template string, lexicon map[string][]*Word) (string, map[string]string) {
+	words := strings.Fields(template)
+	var result []string
+	bindings := map[string]string{}
+	for _, w := range words {
+		result = append(result, chooseWord(w, lexicon, bindings))
+	}
+	return strings.Join(result, " "), bindings
+}
+
+func chooseWord(w string, lexicon map[string][]*Word, bindings map[string]string) string {
+	if !strings.HasPrefix(w, ":") {
+		return w
+	}
+	v := w[1:]
+	i := strings.IndexRune(v, '.')
+	var pos string
+	if i == -1 {
+		pos = v
+	} else {
+		pos = v[:i]
+	}
+	choices := lexicon[pos]
+	if len(choices) == 0 {
+		bindings[v] = "???"
+		return "???"
+	} else {
+		choice := choices[rand.Intn(len(choices))]
+		bindings[v] = choice.Pinyin
+		return choice.English
+	}
+}
+
+func applyBindings(s string, bindings map[string]string) string {
+	words := strings.Fields(s)
+	var result []string
+	for _, w := range words {
+		if !strings.HasPrefix(w, ":") {
+			result = append(result, w)
+		} else if b, ok := bindings[w[1:]]; ok {
+			if b == "" {
+				log.Fatalf("empty string for %s", w[1:])
+			}
+			result = append(result, b)
+		} else {
+			log.Fatalf("no binding for %q", w[1:])
+		}
+	}
+	return strings.Join(result, " ")
 }
 
 func yorn(prompt string) bool {
@@ -104,4 +194,35 @@ func yorn(prompt string) bool {
 	}
 	log.Fatal("too many tries")
 	return false
+}
+
+// Read a tab-separated value file.
+// Require exactly itemsPerLine items per line.
+// Collapse multiple adjacent tabs.
+// Ignore blank lines or lines beginning with '#'.
+func readTSVFile(filename string, itemsPerLine int) ([][]string, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	var lines [][]string
+	s := bufio.NewScanner(f)
+	n := 0
+	for s.Scan() {
+		n++
+		line := strings.TrimSpace(s.Text())
+		if line == "" || line[0] == '#' {
+			continue
+		}
+		parts := strings.FieldsFunc(s.Text(), func(r rune) bool { return r == '\t' })
+		if len(parts) != itemsPerLine {
+			return nil, fmt.Errorf("%s:%d: need %d parts per line, got %d", filename, n, itemsPerLine, len(parts))
+		}
+		lines = append(lines, parts)
+	}
+	if s.Err() != nil {
+		return nil, s.Err()
+	}
+	return lines, nil
 }
---
+++ quiz_main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestChooseWord(t *testing.T) {
+	lexicon := map[string][]*Word{
+		"one": []*Word{{English: "oink", Pinyin: "OINK"}},
+	}
+	for _, test := range []struct {
+		in      string
+		want    string
+		wantKey string
+		wantVal string
+	}{
+		{"foo", "foo", "", ""},
+		{":zero", "???", "zero", "???"},
+		{":one", "oink", "one", "oink"},
+		{":one.2", "oink", "one.2", "oink"},
+	} {
+		bs := map[string]string{}
+		got := chooseWord(test.in, lexicon, bs)
+		if got != test.want {
+			t.Errorf("%s: got %q, want %q", test.in, got, test.want)
+		}
+		if test.wantKey == "" && len(bs) != 0 {
+			t.Errorf("%s: got %d bindings, wanted none", test.in, len(bs))
+		}
+		if test.wantKey != "" && (len(bs) != 1 || bs[test.wantKey] != test.wantVal) {
+			t.Errorf("want %s => %s, got %+v", test.wantKey, test.wantVal, bs)
+		}
+	}
+}
